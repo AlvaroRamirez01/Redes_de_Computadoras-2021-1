d3a57a2cb4a72d35aeb5153ce121be23bac0abdc
Alvaro Ramirez (ThinkPad-T440P-Linux) <alvaro_rl@ciencias.unam.mx>
2021-10-02 00:07:49 -0500

    Se agrego el metodo suma

--- ChatBot/app.py
+++ ChatBot/app.py
@@ -2,6 +2,8 @@
 
 from flask import Flask
 from flask.json import jsonify
+from flask.scaffold import _matching_loader_thinks_module_is_package
+from flask.wrappers import Request
 
 app = Flask(__name__)
 
@@ -14,6 +16,13 @@ def hola():
 def saludo(username):
     response_mensaje = "Hola %s, bienvenido a la pagina de prueba" % username
     return response_mensaje
+
+@app.route('/suma',methods=["POST"])
+def suma():
+    valor = request.json.get("variable")
+    valor = valor +1
+    return jsonify({"variable", valor})
+    
 
 if __name__== "__main__":
     app.run(host="localhost", debug=True)
