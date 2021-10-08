693fa9dbfd76480a1faee4f395d9736d054bf799
Alvaro Ramirez (ThinkPad-T440P-Linux) <alvaro_rl@ciencias.unam.mx>
2021-10-08 16:27:53 -0500

    implementacion de practica 1

--- Practica1/cliente.py
+++ Practica1/cliente.py
@@ -1,58 +1,19 @@
-from getmac import get_mac_address,getmac
-from typing import cast
+from getmac import get_mac_address
 import requests
-from decouple import config
+import psutil
 import json as js
+from typing import cast
+from decouple import config
+
 
 URL_1 = config("LOCALHOST", cast=str)
-URL_2 = config("LOCALHOST", cast=str)
-URL_3 = config("LOCALHOST", cast=str)
+URL_2 = config("INVITADO1", cast=str)
+URL_3 = config("INVITADO2", cast=str)
 
+def consultarRAM():
+    memory = psutil.virtual_memory().total / (1024.0 ** 3)
+    print(memory)
+    return memory
 
-i = 0
-valor = 0
-while i < 50:
-    if i %2 == 0:
-        response = requests.post(URL_1+"/suma", json={"variable": valor})
-        valor = response.json().get("variable")
-        print(valor)
-        
-        i = i + 1
-    else:
-        response = requests.post(URL_2+"/suma", json={"variable": valor})
-        valor = response.json().get("variable")
-        print(valor)
-        i = i + 1
-
-
-
-
-# ejemplos para usar la mac
-
-''' eth_mac = get_mac_address(interface="eth0")
-win_mac = get_mac_address(interface="Ethernet 3")
-ip_mac = get_mac_address(ip="192.168.0.1")
-ip6_mac = get_mac_address(ip6="::1")
-host_mac = get_mac_address(hostname="localhost")
-updated_mac = get_mac_address(ip="10.0.0.1", network_request=True) '''
-
-# Changing the port used for updating ARP table (UDP packet)
-
-#getmac.PORT = 44444  # Default: 55555
-#print(getmac.get_mac_address(ip="192.168.0.1", network_request=True))
-
-# Enabling debugging
-
-#getmac.DEBUG = 2  # DEBUG level 2
-#print(getmac.get_mac_address(interface="Ethernet 3"))
-
-#import psutil
-
-# gives a single float value
-#psutil.cpu_percent()
-# gives an object with many fields
-#psutil.virtual_memory()
-# you can convert that object to a dictionary 
-#print (psutil.virtual_memory().total)
-#memory = psutil.virtual_memory().total / (1024.0 ** 3)
-#print(memory)
+def getMAC():
+    return get_mac_address()
--- Practica1/servidor.py
+++ Practica1/servidor.py
@@ -1,18 +1,46 @@
-from flask import Flask, json, jsonify, request
-from utils import suma_peticion
+import requests
+from flask import Flask, request
+from flask.json import jsonify
+from getmac import get_mac_address
+from decouple import config
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
-def recibe():
-    return jsonify({"variable": 0})
+URL_1 = config("LOCALHOST", cast=str)
+URL_2 = config("INVITADO_1", cast=str)
+URL_3 = config("INVITADO_2", cast=str)
 
-@app.route("/suma", methods= ["POST"])
-def suma():
-    resultado = suma_peticion(request)
-    print('servidor respondiendo ' + str(resultado.get("variable")))
-    return jsonify(resultado)
+mac = get_mac_address()
+print(mac)
+name = "alvaro"
+
+@app.route("/", methods=["POST"])
+def recibe():
+    request_inicial = request.get_json()
+    mensaje = "El ciclo de suma comenzo en:"+str(URL_1)
+    print("Valor actualmente: " + str(request_inicial['valor']))
+    print("Enviando al siguiente EndPoint..." + URL_1)
+    if(request_inicial['valor'] < 50):
+        # esta linea es la que va a cambiar dependiendo del servidor del usuario
+        requests.post(URL_1, json={"valor": request_inicial['valor'] + 1, "mac": mac, "name": name})
+    else:
+        print("Se finalizo la suma")
+        requests.post(URL_1 + "/finish", json={"valor": request_inicial['valor'], "mac": mac, "name": name})
+        requests.post(URL_2 + "/finish", json={"valor": request_inicial['valor'], "mac": mac, "name": name})
+        requests.post(URL_3 + "/finish", json={"valor": request_inicial['valor'], "mac": mac, "name": name})
+    return jsonify({"message": mensaje, "statusCode": 200, "mac": mac, "name": name})
+
+@app.route("/finish", methods=["POST"])
+def finish():
+    request_data = request.get_json()
+    finish_valor = request_data['valor']
+    finish_mac = request_data['mac']
+    finish_name = request_data['name']
+    print("El ciclo ha terminado con el valor: " + str(finish_valor))
+    print("La mac de quien termino el ciclo es: " + str(finish_mac))
+    print("El nombre de quien termino el ciclo es: " + str(finish_name))
+    return jsonify({"message": "El ciclo ha terminado", "statusCode": 200})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run("localhost",debug=True)
     
