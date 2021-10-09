b43c5642b2210d6de5aa8bd2cfefd83e691e7828
Alvaro Ramirez (ThinkPad-T440P-Linux) <alvaro_rl@ciencias.unam.mx>
2021-10-09 17:25:36 -0500

    actualizacion de directorio

--- Practica1/cliente.py
+++ Practica1/cliente.py
@@ -10,10 +10,24 @@ URL_1 = config("LOCALHOST", cast=str)
 URL_2 = config("INVITADO1", cast=str)
 URL_3 = config("INVITADO2", cast=str)
 
-def consultarRAM():
-    memory = psutil.virtual_memory().total / (1024.0 ** 3)
-    print(memory)
-    return memory
+def checaRAM():
+    ram1 = psutil.virtual_memory().total / (1024.0 ** 3)
+    ram2 = requests.get(URL_2 + "/get-ram").json()['ram']
+    ram3 = requests.get(URL_3 + "/getRam").json()['ram']
+    response = ""
+    if(ram1 >= ram2 and ram1 >= ram3):
+        response = requests.post(
+            URL_1, json={"valor": 0, "mac": mac, "name": "Alvaro"})
+    elif(ram2 > ram1 and ram2 > ram3):
+        response = requests.post(
+            URL_2, json={"valor": 0, "mac": mac, "name": "Eric"})
+    else:
+        response = requests.post(
+            URL_3, json={"valor": 0, "mac": mac, "name": "Dan"})
+    print(response.json())
 
 def getMAC():
     return get_mac_address()
+
+checaRAM()
+getMAC()
--- Practica1/servidor.py
+++ Practica1/servidor.py
@@ -1,8 +1,13 @@
+''' Practica 1: Redes y tuneles
+Autor: Alvaro Ramirez Lopez
+Numero de cuenta: 316276355 '''
+
 import requests
 from flask import Flask, request
 from flask.json import jsonify
 from getmac import get_mac_address
 from decouple import config
+import psutil
 
 app = Flask(__name__)
 
@@ -11,7 +16,6 @@ URL_2 = config("INVITADO_1", cast=str)
 URL_3 = config("INVITADO_2", cast=str)
 
 mac = get_mac_address()
-print(mac)
 name = "alvaro"
 
 @app.route("/", methods=["POST"])
@@ -19,14 +23,12 @@ def recibe():
     request_inicial = request.get_json()
     mensaje = "El ciclo de suma comenzo en:"+str(URL_1)
     print("Valor actualmente: " + str(request_inicial['valor']))
-    print("Enviando al siguiente EndPoint..." + URL_1)
+    print("Enviando al siguiente EndPoint..." + URL_2)
     if(request_inicial['valor'] < 50):
         # esta linea es la que va a cambiar dependiendo del servidor del usuario
-        requests.post(URL_1, json={"valor": request_inicial['valor'] + 1, "mac": mac, "name": name})
+        requests.post(URL_2, json={"valor": request_inicial['valor'] + 1, "mac": mac, "name": name})
     else:
         print("Se finalizo la suma")
-        requests.post(URL_1 + "/finish", json={"valor": request_inicial['valor'], "mac": mac, "name": name})
-        requests.post(URL_2 + "/finish", json={"valor": request_inicial['valor'], "mac": mac, "name": name})
         requests.post(URL_3 + "/finish", json={"valor": request_inicial['valor'], "mac": mac, "name": name})
     return jsonify({"message": mensaje, "statusCode": 200, "mac": mac, "name": name})
 
@@ -40,6 +42,11 @@ def finish():
     print("La mac de quien termino el ciclo es: " + str(finish_mac))
     print("El nombre de quien termino el ciclo es: " + str(finish_name))
     return jsonify({"message": "El ciclo ha terminado", "statusCode": 200})
+
+@app.route("/get-ram", methods=["GET"])
+def get_ram():
+    ramDisponible = psutil.virtual_memory().total / (1024.0 ** 3)
+    return jsonify({"message": "La cantidad de memoria ram aparecerá a continuación", "statusCode": 200, "ram": ramDisponible})
 
 if __name__ == '__main__':
     app.run("localhost",debug=True)
