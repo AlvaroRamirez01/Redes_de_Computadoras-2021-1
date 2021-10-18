c9d59c2afffccd881288a5f1985bafbda00d8166
Alvaro Ramirez(Alvaro-Desktop-Linux) <alvaro_rl@ciencias.unam.mx>
2021-10-18 02:12:54 -0500

    modificacion de declaracion de lista

--- Tarea1/Actividad1/Domino.java
+++ Tarea1/Actividad1/Domino.java
@@ -4,6 +4,8 @@ import java.util.LinkedList;
 
 public class Domino {
     
+    private static LinkedList<Ficha> lista1;
+
     public static void main(String[] args) {
         Ficha ficha_1_2 = new Ficha(1,2);
         Ficha ficha_1_3 = new Ficha(1,3);
@@ -12,14 +14,14 @@ public class Domino {
 
         System.out.println(ficha_1_2.toString());
 
-        LinkedList lista1 = new LinkedList<Ficha>();
+        lista1 = new LinkedList<Ficha>();
 
         lista1.add(ficha_1_2);
         lista1.add(ficha_1_3);
         lista1.add(ficha_1_4);
 
         while (!(lista1.isEmpty())) {
-            Ficha n = (Ficha) lista1.getFirst();
+            Ficha n = lista1.getFirst();
             System.out.println(n.toString());
             lista1.removeFirst();
         }
