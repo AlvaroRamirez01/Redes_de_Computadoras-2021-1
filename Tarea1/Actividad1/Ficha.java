18bd85dd3b8d5debf88357ec38728979d1bbf39f
Alvaro Ramirez(Alvaro-Desktop-Linux) <alvaro_rl@ciencias.unam.mx>
2021-10-18 02:09:18 -0500

    Se definio la clase ficha con su comportamiento

--- Tarea1/Actividad1/Ficha.java
+++ Tarea1/Actividad1/Ficha.java
@@ -26,4 +26,45 @@ public class Ficha {
     public int getCara1(){
         return cara1;
     }
+
+    /**
+     * Modifica la cara 1 de la ficha.
+     */
+    public void setCara1(int cara1){
+        this.cara1 = cara1;
+    }
+
+    /**
+     * Regresa la cara 2 de la ficha.
+     * @return la cara 2 de la ficha.
+     */
+    public int getCara2(){
+        return cara2;
+    }
+
+    /**
+     * Modifica la cara 2 de la ficha.
+     */
+    public void setCara2(int cara2){
+        this.cara2 = cara2;
+    }
+
+    /**
+     * Regresa la representacion en cadena de una ficha.
+     */
+    @Override public String toString(){
+        String cadena = String.format("[%d,%d]", this.cara1,this.cara2);
+        return cadena;
+    }
+
+    /**
+     * Nos permite comparar si la ficha recibida es igual a la ficha que 
+     * mando a llamar el metodo.
+     * @param ficha2 TODO
+     */
+    public boolean equals(Ficha ficha2){
+        return (this == null || !(ficha2.cara1 != (cara1)) || !(ficha2.cara2 != (cara2))) 
+            ? false : true;
+    }
+
 }
