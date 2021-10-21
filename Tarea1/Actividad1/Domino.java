d34de4a19b666df52202283994249963a891e907
Alvaro Ramirez(Alvaro-Desktop-Linux) <alvaro_rl@ciencias.unam.mx>
2021-10-20 22:42:34 -0500

    modelado del tablero en proceso

--- Tarea1/Actividad1/Domino.java
+++ Tarea1/Actividad1/Domino.java
@@ -4,12 +4,20 @@ import java.util.LinkedList;
 
 public class Domino {
     
-    private static LinkedList<Ficha> lista1;
+    private static LinkedList<Ficha> lista1 = new LinkedList<Ficha>();
 
     public static void main(String[] args) {
-        //Ficha ficha_1_2 = new Ficha(1,2);
-        //Ficha ficha_1_3 = new Ficha(1,3);
-        //Ficha ficha_1_4 = new Ficha(1,4);
+
+        generaFichas();
+        
+        
+
+    }
+    /**
+     * Este metodo nos ayuda a generar las fichas al inicio de una nueva partida.
+     */
+    private static void generaFichas(){
+        // creamos las fichas de domino
         Ficha ficha_0_0= new Ficha(0,0);
         Ficha ficha_0_1= new Ficha(0,1);
         Ficha ficha_0_2= new Ficha(0,2);
@@ -45,6 +53,11 @@ public class Domino {
         
         Ficha ficha_6_6= new Ficha(6,6);
 
+        /**
+         * Agregamos las fichas a la lista la cual nos ayudara a 
+         * hacer las funciones de revolver fichas y de repartir fichas
+         * al inicio de la partida y cuando querramos comer mas fichas.
+         */
         lista1.add(ficha_0_0);
         lista1.add(ficha_0_1);
         lista1.add(ficha_0_2);
@@ -53,25 +66,38 @@ public class Domino {
         lista1.add(ficha_0_5);
         lista1.add(ficha_0_6);
 
+        lista1.add(ficha_1_1);
+        lista1.add(ficha_1_2);
+        lista1.add(ficha_1_3);
+        lista1.add(ficha_1_4);
+        lista1.add(ficha_1_5);
+        lista1.add(ficha_1_6);
 
-        System.out.println(ficha_1_2.toString());
-        int aux = ficha_1_2.getCara2();
-        ficha_1_2.setCara2(ficha_1_2.getCara1());
-        ficha_1_2.setCara1(aux);
-        System.out.println(ficha_1_2.toString());
+        lista1.add(ficha_2_2);
+        lista1.add(ficha_2_3);
+        lista1.add(ficha_2_4);
+        lista1.add(ficha_2_5);
+        lista1.add(ficha_2_6);
 
-        lista1 = new LinkedList<Ficha>();
+        lista1.add(ficha_3_3);
+        lista1.add(ficha_3_4);
+        lista1.add(ficha_3_5);
+        lista1.add(ficha_3_6);
 
-        //lista1.add(ficha_1_2);
-        //lista1.add(ficha_1_3);
-        //lista1.add(ficha_1_4);
+        lista1.add(ficha_4_4);
+        lista1.add(ficha_4_5);
+        lista1.add(ficha_4_6);
 
-        //while (!(lista1.isEmpty())) {
-        //    Ficha n = lista1.getFirst();
-        //    System.out.println(n.toString());
-        //    lista1.removeFirst();
-        //}
+        lista1.add(ficha_5_5);
+        lista1.add(ficha_5_6);
 
+        //System.out.println(ficha_5_6.toString());
+        //ficha_5_6.giraFicha(ficha_5_6.getCara1(),ficha_5_6.getCara2());
+        //System.out.println(ficha_5_6.toString());
+
+        for (Ficha ficha : lista1) {
+            System.out.print(","+ficha.toString());
+        }
     }
     
 }
